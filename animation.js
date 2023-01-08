2598ece227a65d8c0d112a9fba8392e1a38ded55
hamzona <hamzadjedovic2000@gmail.com>
2023-01-08 16:28:40 +0100

    novi

---
+++ animation.js
@@ -0,0 +1,39 @@
+let buttonsRight = document.querySelector(".img-button.right");
+let buttonLeft = document.querySelector(".img-button.left");
+let slides = document.querySelectorAll(".imges");
+let i = 0;
+buttonsRight.addEventListener("click", () => {
+  displayNone();
+  i++;
+
+  if (i > slides.length - 1) {
+    i = 0;
+  }
+  slides[i].classList.add("show");
+});
+
+buttonLeft.addEventListener("click", () => {
+  displayNone();
+  i--;
+
+  if (i < 0) {
+    i = slides.length - 1;
+  }
+  slides[i].classList.add("show");
+});
+
+setInterval(() => {
+  displayNone();
+  i++;
+  if (i > slides.length - 1) {
+    i = 0;
+  }
+  console.log(i);
+  slides[i].classList.add("show");
+}, 5000);
+
+function displayNone() {
+  slides.forEach((slide) => {
+    slide.classList.remove("show");
+  });
+}
